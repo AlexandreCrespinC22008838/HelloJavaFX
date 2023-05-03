bb1324fa0188580194144bd1116f242728f06977
AlexandreCrespinC22008838 <alexandrecrespin2004@gmail.com>
2023-05-03 09:36:22 +0200

    1.2 - Conception d'une premiere interface graphique basique

--- src/main/java/com/example/partie1/MaPremiereClasseJavaFX.java
+++ src/main/java/com/example/partie1/MaPremiereClasseJavaFX.java
@@ -1,40 +1,83 @@
 package com.example.partie1;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MaPremiereClasseJavaFX extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        // Création du conteneur principal
+        VBox vbox = new VBox();
+
+        // Création du conteneur correspondant à la ligne de contrôle haut dessus du tableau
+        HBox topControls = new HBox();
+        topControls.setAlignment( Pos.BOTTOM_LEFT );
+        Button btnRefresh = new Button("Refresh");
+
+        HBox topRightControls = new HBox();
+        HBox.setHgrow(topRightControls, Priority.ALWAYS );
+        topRightControls.setAlignment( Pos.BOTTOM_RIGHT );
+        Hyperlink signOutLink = new Hyperlink("Sign Out");
+        topRightControls.getChildren().add( signOutLink );
+
+        topControls.getChildren().addAll( btnRefresh, topRightControls );
+
+        // Création du tableau avec les deux colonnes
+        TableView<Object> tblCustomers = new TableView<>();
+        tblCustomers.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        TableColumn<Object, String> lastNameCol = new TableColumn<>("Last Name");
+        TableColumn<Object, String> firstNameCol = new TableColumn<>("First Name");
+        tblCustomers.getColumns().addAll( lastNameCol, firstNameCol );
+        VBox.setVgrow( tblCustomers, Priority.ALWAYS );
+
+        // Création de la ligne de séparation
+        Separator sep = new Separator();
+
+        // Création du bandeau en bas de la fenêtre
+        HBox bottomControls = new HBox();
+        bottomControls.setAlignment(Pos.BOTTOM_RIGHT );
+        Button btnClose = new Button("Close");
+        bottomControls.getChildren().add( btnClose );
+
+        // Ajout des contrôleurs au conteneur principal
+        vbox.getChildren().addAll(
+                topControls,
+                tblCustomers,
+                sep,
+                bottomControls
+        );
+
+        // Ajout du conteneur à la scene
+        Scene scene = new Scene(vbox );
+
+        // Ajout de la scene à la fenêtre et changement de ses paramètres (dimensions et titre)
+        primaryStage.setScene( scene );
+        primaryStage.setWidth( 800 );
+        primaryStage.setHeight( 600 );
+        primaryStage.setTitle("VBox and HBox App");
+
+        VBox.setMargin( topControls, new Insets(10.0d) );
+        VBox.setMargin( tblCustomers, new Insets(0.0d, 10.0d, 10.0d, 10.0d) );
+        VBox.setMargin( bottomControls, new Insets(10.0d) );
+
+        // Affichage de la fenêtre
+        primaryStage.show();
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        // Récupérer les arguments passés en ligne de commande
-        Parameters params = getParameters();
-        int width = 300;
-        int height = 150;
-
-        if (params.getRaw().size() > 0) {
-            // Utiliser le premier argument passé en ligne de commande comme largeur de la fenêtre
-            width = Integer.parseInt(params.getRaw().get(0));
-        }
-
-        if (params.getRaw().size() > 1) {
-            // Utiliser le deuxième argument passé en ligne de commande comme hauteur de la fenêtre
-            height = Integer.parseInt(params.getRaw().get(1));
-        }
-
-        // Créer une scène
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, width, height);
-
-        // Définir la scène principale de l'application
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Exemple d'utilisation des arguments en ligne de commande");
-        primaryStage.show();
+    public static void main(String[] args) {
+        launch(args);
     }
 }
